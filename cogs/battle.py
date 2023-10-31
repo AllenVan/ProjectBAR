f120d0665e9778756c1a911be7751eea9036d595
AllenVan <allenkotetsu@yahoo.com>
2023-10-30 23:04:45 -0700

    slight refactor to reduce redundant code. A global function now exists to help reduce redundant lines for messages when a combatant dies

--- cogs/battle.py
+++ cogs/battle.py
@@ -3,12 +3,24 @@ from discord.ext import commands
 from discord.ui import Select, View
 import random
 
+def battle_end(ctx, enemy_killed):
+		if enemy_killed:
+			temp_message=discord.Embed(title=f'The enemy has been slain!', color=discord.Color.green())
+			temp_message.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+			temp_message.set_footer(text=f"You live to fight another day...")
+		else:
+			temp_message=discord.Embed(title=f"You Died!", color=discord.Color.red())
+			temp_message.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+			temp_message.set_footer(text=f"Better luck next time...")
+		return temp_message
 
 class battleSystem(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 		self.client.enemy = None
-	
+		self.player_message = None
+		self.enemy_message = None
+
 	def spawn(self):
 		self.client.enemy = random.choice(self.client._spawns['mobs']).copy()
 
@@ -18,56 +30,46 @@ class battleSystem(commands.Cog):
 
 	@commands.command()
 	async def battle(self, ctx):
+		
 		if self.client.player == None:
 			await ctx.send("You have not selected a job. Use `.start` command")
 
 		else:
 			async def callback(interaction):
-				# select_menu.values is simply an array of selections made by the user
-				# user can only choose 1 option so we use the first and only value in the array
-				
-				view = View()
-				p_message, e_damage = getattr(self.client.combat_skills, select_menu.values[0])()  # calls a function in skills.py class with the same name
-				e_message, p_damage = getattr(self.client.combat_skills, random.choice(self.client.enemy['skills']))()
+				self.player_message, e_damage = getattr(self.client.combat_skills, select_menu.values[0])()  # calls a function in skills.py class with the same name
+				self.enemy_message, p_damage = getattr(self.client.combat_skills, random.choice(self.client.enemy['skills']))()
 				
 				if self.client.enemy["HP"] - e_damage <= 0:
-					embed_message=discord.Embed(title=f'The {self.client.enemy["name"]} is slain!', color=discord.Color.green())
-					footer_text=f'You live to fight another day...'
+					embed_message=battle_end(ctx, True)
 					self.client.enemy = None
+					view.remove_item(select_menu)
+					await interaction.response.send_message(embed=embed_message, view=view, ephemeral=False)
 				elif self.client.player["HP"] - p_damage <= 0:
-					embed_message=discord.Embed(title=f"You Died!", color=discord.Color.red())
-					footer_text=f"Better luck next time..."
+					embed_message=battle_end(ctx, False)
 					self.client.player = None
 					self.client.enemy = None
+					view.remove_item(select_menu)
+					await interaction.response.send_message(embed=embed_message, view=view, ephemeral=False)
 				else:
 					self.client.player["HP"] -= p_damage
 					self.client.enemy["HP"] -= e_damage
-					embed_message=discord.Embed(title=f"Battle!", color=discord.Color.dark_gray())
-					embed_message.add_field(name="Player", value=p_message, inline=True)
-					embed_message.add_field(name="Enemy", value=e_message, inline=True)
-					footer_text=f'What will you do? Distance is {self.client.combat_skills.distance}. Your HP is {self.client.player["HP"]}. Enemy HP is {self.client.enemy["HP"]}.'
-					select_menu.callback = callback
-					view.add_item(select_menu)
+					await ctx.reinvoke()
+					await interaction.response.defer() # "closes" the current embed interaction now that reinvoked is called
+					
 				
-				embed_message.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
-				embed_message.set_footer(text=footer_text)
-				await interaction.response.send_message(embed=embed_message, view=view, ephemeral=False)
-
 			if self.client.enemy:
 				embed_message=discord.Embed(title=f'The {self.client.enemy["name"]} did not hear no bell.', color=discord.Color.light_gray())
+				embed_message.add_field(name="Player", value=self.player_message, inline=True)
+				embed_message.add_field(name="Enemy", value=self.enemy_message, inline=True)
 			else:
 				self.spawn()
 				embed_message=discord.Embed(title=f'The {self.client.enemy["name"]} ambushes you!', color=discord.Color.light_gray())
+				embed_message.set_image(url=self.client.enemy['image'])
 
 			embed_message.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
-			embed_message.set_image(url=self.client.enemy['image'])
 			embed_message.set_footer(text=f'What will you do? Distance is {self.client.combat_skills.distance}. Your HP is {self.client.player["HP"]}. Enemy HP is {self.client.enemy["HP"]}.')
 
-			select_menu = Select(options=[])
-			for item in self.client.player["skills"].values():
-				select_menu.append_option(discord.SelectOption(
-					label=item
-				))
+			select_menu = Select(options=[discord.SelectOption(label=item) for item in self.client.player["skills"].values()])
 
 			select_menu.callback = callback
 			view = View()
